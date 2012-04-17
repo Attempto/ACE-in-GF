fa87d6570af590ccb9f0d91bfb5c4b0a49b65b43
Kaarel Kaljurand <kaljurand@gmail.com>
2012-04-17 11:24:11 +0200

    Fix small bug in diff_rg.bash

--- tools/diff_rg.bash
+++ tools/diff_rg.bash
@@ -1,6 +1,9 @@
-# Simple checking of the differences between the ACE and the English resource grammar.
+# Simple checking of the differences between the ACE and the English resource grammars.
 # Expects the environment variable $GF_RGL_SRC to point to the source of
-# GF Resource Grammar Library.
+# the GF Resource Grammar Library.
+
+# @author Kaarel Kaljurand
+# @version 2012-04-17
 
 # Usage:
 # bash diff_rg.bash | bash
@@ -11,5 +14,5 @@ echo "ace_rg_src=\"${ace_rg_src}\""
 echo "eng_rg_src=\${GF_RGL_SRC}/english/"
 echo "api_rg_src=\${GF_RGL_SRC}/api/"
 
-ls ${ace_rg_src}ace/*.gf | sed "s/.*\/\(.*\)Ace\(.*\)\.gf/diff \${ace_rg_src}ace\/\1Ace.gf \${eng_rg_src}\1Eng\2\.gf/"
-ls ${ace_rg_src}api/*.gf | sed "s/.*\/\(.*\)Ace\(.*\)\.gf/diff \${ace_rg_src}api\/\1Ace.gf \${api_rg_src}\1Eng\2\.gf/"
+ls ${ace_rg_src}ace/*.gf | sed "s/.*\/\(.*\)Ace\(.*\)\.gf/diff \${eng_rg_src}\1Eng\2\.gf \${ace_rg_src}ace\/\1Ace\2.gf/"
+ls ${ace_rg_src}api/*.gf | sed "s/.*\/\(.*\)Ace\(.*\)\.gf/diff \${api_rg_src}\1Eng\2\.gf \${ace_rg_src}api\/\1Ace\2.gf/"
