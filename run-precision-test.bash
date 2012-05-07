cd9cff7d9dcbdbb958b6703089cc900070422e2d
Kaarel Kaljurand <kaljurand@gmail.com>
2012-05-07 18:04:44 +0200

    Tweak the parameters of random generation a bit

--- run-precision-test.bash
+++ run-precision-test.bash
@@ -9,13 +9,19 @@ pgf=ACE-0_0_2.pgf
 trees_file=build/gr/precision-test.txt
 out=test_precision_out.txt
 out_fail=test_precision_out_fail.txt
+cat="Text"
+# At depth=3 gr can reliably generate 1000s of trees,
+# at higher depths it seems to hang.
+depth="3"
+# Exclude questions for the time being.
+tree="(baseText (sText ?))"
 
 if [ $# -eq 1 ]; then
     if [[ $1 =~ ^[0-9]+$ ]]; then
         # Argument was a number, so we generate new trees
         gr_number=$1
-        echo "Generating ${gr_number} trees..."
-        echo "gr -cat=Text -number=${gr_number} -depth=10 | l -lang=Ace -bind" | gf --run ${pgf} > ${trees_file}
+        echo "Generating ${gr_number} trees for category ${cat} ..."
+        echo "gr -cat=${cat} -number=${gr_number} -depth=${depth} ${tree} | l -lang=Ace -bind" | gf --run ${pgf} > ${trees_file}
     else
         # Treat argument as a filename
         trees_file=$1
