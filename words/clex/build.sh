d41729c3c2387912ca9ef44cfadb384520cfe961
Dan Haub <haub@chapman.edu>
2021-08-06 22:40:32 +0000

    began building linearizations for cles

--- words/clex/build.sh
+++ words/clex/build.sh
@@ -2,11 +2,10 @@ clex='./clex_lexicon.pl'
 
 ace="../../lib/src/ace/"
 api="../../lib/src/api/"
-grammar="../../grammars/ace/"
+grammar="../../grammars/ace/:../../grammars/acewiki_aceowl/"
 
 path="present:${grammar}:${ace}:${api}"
 
-
-swipl -f none -g "main('$clex')" -t halt -s clex_to_gf.pl
+# swipl -f none -g "main('$clex')" -t halt -s clex_to_gf.pl
 
 gf +RTS -K100M -RTS --preproc=mkPresent --make --optimize-pgf --path $path Clex*.gf
